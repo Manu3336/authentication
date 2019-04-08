eb3a69f3c11b0aada26a3d6e1ecc47d714c83271
Manu Kapoor <manukapoor@l-156132681.wipro.com>
2019-04-08 19:17:47 +0530

    aa

---
+++ config/keys.js
@@ -0,0 +1,19 @@
+module.exports = {
+    google:
+    {
+        googleClientID: '472157874882-l5o7f18qu82iudsn1772n1q6rrh4geub.apps.googleusercontent.com',
+        googleClientSecret: 'a84uPUWXclm9qWF0IVWFoISm'
+    },
+    facebook:{
+        facebookAppID:'1128317990673363',
+        facebookAppSecret:'db91886ff171f8534625eb304b9b91d4'
+    },
+    mongodb:{
+        dbURI:"mongodb://root:root1234@ds133086.mlab.com:33086/authentication-dataset"
+        // dbURI:'https://api.mlab.com/api/1/databases/authentication-dataset/collections/users?apiKey=wdZnzSMriVW6RcYMnyfvx9DEdwQQENQn&fbclid=IwAR0GSEqlQWhqgD9xrbqG5GfKeqc_DZFGgn8BP9k5C0vTb92Tug6WZvHKjOM'
+    },
+    session:{
+        cookieKey: "thiIsARandomString"
+    }
+};
+
